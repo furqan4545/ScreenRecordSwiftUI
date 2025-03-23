fc942284495107361c3f47b3cb7822f6a030c5d0
Furqan Ali <furqan.ali9500@gmail.com>
2025-03-24 02:04:27 +0900

    Initial Commit

---
+++ ScreenRecord/ScreenRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenRecordApp.swift
+//  ScreenRecord
+//
+//  Created by Furqan Ali on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
